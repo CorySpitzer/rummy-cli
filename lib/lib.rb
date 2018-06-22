3a33a59177df92b2efa8c15203c6d3148f005500
Cory Spitzer <coryspitzer@gmail.com>
2018-06-22 16:36:04 -0400

    Fix test again

--- lib/lib.rb
+++ lib/lib.rb
@@ -4,5 +4,5 @@ class Card
         @rank = rank
     end
 
-    attr_reader :suit#, :rank    
+    # attr_reader :suit, :rank    
 end
--- spec/_spec.rb
+++ spec/_spec.rb
@@ -12,5 +12,9 @@ describe Card do
         it "has a suit" do
             expect(Card.new('hearts', 3).suit).to eq 'hearts'
         end
+
+        it "has a rank" do
+            expect(Card.new('hearts', 3).rank).to eq 3
+        end
     end
 end
